f6267525691d43a2783060e1e137b77b4aa43026
manasa-2007 <mullapudi.manasa02@gmail.com>
2026-06-16 12:45:29 +0530

    Fix Render port binding

--- webapp/app.py
+++ webapp/app.py
@@ -266,14 +266,10 @@ def predict():
 
 if __name__ == "__main__":
 
-    port = int(
-        os.environ.get(
-            "PORT",
-            5000
-        )
-    )
+    port = int(os.environ.get("PORT", 5000))
 
     app.run(
         host="0.0.0.0",
-        port=port
+        port=port,
+        debug=False
     )
